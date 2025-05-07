0e52839139e0fec184e36774decea288e80d961f
Mateusz Praniuk <praniukmateusz@gmail.com>
2025-05-07 21:03:07 +0200

    test2

--- test_app/app.py
+++ test_app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from your Python app! This text was automatically deployed after push :)"
+    return "Hello from your Python app! This text was automatically deployed after push :)))"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
