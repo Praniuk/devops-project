7b8e98aecd32987a30e169bf7f5659bd0b2bc367
Mateusz Praniuk <praniukmateusz@gmail.com>
2025-05-07 21:24:11 +0200

    test2

--- test_app/app.py
+++ test_app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from your Python app! This is new text after push :OO"
+    return "Hello from your Python app! This is new text after push :O"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
